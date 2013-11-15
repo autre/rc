34d599663733314df6ada33dd80bc24b033bcf0f
autre <b.alexandrou@gmail.com>
2013-11-15 16:46:45 +0200

    adding .config files

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -434,3 +434,8 @@ end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+
+
+globalkeys = awful.util.table.join(globalkeys,
+    awful.key({}, "Print", function () awful.util.spawn_with_shell("touch ~/bar") end)
+)
